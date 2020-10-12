0bb63fc317eae3287f9611b6186f8b5c8cc76676
MassimoEsperto <em.decarlo@gmail.com>
2020-10-12 17:07:00 +0200

    Update framework.module.ts

--- src/app/module/framework.module.ts
+++ src/app/module/framework.module.ts
@@ -1,9 +1,10 @@
 
 import {NgModule} from '@angular/core';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
 
 
 const modules = [
-     
+  MDBBootstrapModule.forRoot()
     ]
 
 @NgModule({
