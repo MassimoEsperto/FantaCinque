829627f8507e700191401a181df71de561c095cf
MassimoEsperto <em.decarlo@gmail.com>
2020-10-12 17:07:11 +0200

    Create my-button.component.ts

---
+++ src/app/components/my-button/my-button.component.ts
@@ -0,0 +1,24 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'my-button',
+  templateUrl: './my-button.component.html',
+  styleUrls: ['./my-button.component.scss']
+})
+export class MyButtonComponent implements OnInit {
+
+  @Input() submit=false;
+  @Input() disabled = false;
+  @Input() color:string="info";
+  @Input() label:string="Clicca";
+  @Input() block = false;
+  @Input() loading = false;
+  type:string;
+  
+  constructor() { }
+
+  ngOnInit(){
+    this.type=this.submit?"submit":"button";  
+  }
+
+}
