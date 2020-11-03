b4b05c81fdd072d9cdcca1fcb71666076bdce41c
MassimoEsperto <em.decarlo@gmail.com>
2020-11-03 16:16:54 +0100

    Update footer.component.ts

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AdminService } from 'src/app/services/admin.service';
 
 @Component({
   selector: 'my-footer',
@@ -7,9 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
-  constructor() { }
+  constructor(private services: AdminService) { }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(){this.versione=this.services.versione()}
 
+  versione:any;
 }
