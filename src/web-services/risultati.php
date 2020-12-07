845922c5d1f6967746c71d7a5dfc0c3797bfb6d0
MassimoEsperto <em.decarlo@gmail.com>
2020-12-07 17:33:53 +0100

    Update risultati.php

--- src/web-services/risultati.php
+++ src/web-services/risultati.php
@@ -6,7 +6,7 @@ require 'connect_local.php';
     
 $elements = [];
 
-$sql = "SELECT c.id_partita,c.girone,c.giornata,u.squadra as casa,t.squadra as trasferta,r.gol_casa,r.gol_trasferta,r.calcolato,d.data_giornata ";
+$sql = "SELECT c.id_partita,c.girone,c.giornata,u.id_utente as id_casa,u.squadra as casa,t.id_utente as id_trasferta,t.squadra as trasferta,r.gol_casa,r.gol_trasferta,r.calcolato,d.data_giornata ";
 $sql .="FROM calendario c,utenti u,utenti t,risultati r, data_partite d ";
 $sql .="WHERE u.id_utente=c.utente_casa and t.id_utente=c.utente_trasferta and r.id_risultato=c.id_partita and d.giornata=c.giornata ";
 $sql .="ORDER BY  c.giornata,c.girone,c.id_partita ";
@@ -23,7 +23,9 @@ if($result = mysqli_query($con,$sql))
 		$elements[$ele]['giornata'] = $row['giornata'];
         $elements[$ele]['calcolato'] = $row['calcolato'];
 		$elements[$ele]['casa'] = $row['casa'];
+        $elements[$ele]['id_casa'] = $row['id_casa'];
 		$elements[$ele]['trasferta'] = $row['trasferta'];
+		$elements[$ele]['id_trasferta'] = $row['id_trasferta'];
         $elements[$ele]['gol_casa'] = $row['gol_casa'];
 		$elements[$ele]['gol_trasferta'] = $row['gol_trasferta'];
         $elements[$ele]['data'] = $row['data_giornata'];
