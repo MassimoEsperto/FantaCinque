a9870d994711aecbe0a2144020a45bfab44ed84a
MassimoEsperto <em.decarlo@gmail.com>
2020-12-07 17:33:49 +0100

    Update rec_password.php

--- src/web-services/rec_password.php
+++ src/web-services/rec_password.php
@@ -3,7 +3,8 @@
 require 'connect_local.php';
 
 $user = $_GET['user'];($_GET['user'] !== null && $_GET['user'] !== '')? mysqli_real_escape_string($con, $_GET['user']) : false;
-$email = ($_GET['email'] !== null && $_GET['email'] !== '')? mysqli_real_escape_string($con, $_GET['email']) : false;
+$email = $_GET['email'];($_GET['email'] !== null && $_GET['email'] !== '')? mysqli_real_escape_string($con, $_GET['email']) : false;
+
 $pass='';
 
  // Validate.
@@ -13,14 +14,14 @@ if(trim($user) === '' || trim($email) === '')
 }
   
   
-	$subject = "Richiesta Password fantacinque";
+	$subject = "Richiesta Password WorldWideFantashit";
 
 	// Always set content-type when sending HTML email
 	$headers = "MIME-Version: 1.0" . "\r\n";
 	$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
 
 	// More headers
-	$headers .= 'From: <fantacinque@shitcup.com>' . "\r\n";
+	$headers .= 'From: <WorldWideFantashit@shitcup.com>' . "\r\n";
 
 
 
@@ -71,12 +72,12 @@ else
                 die(json_encode(array('message' => $e->getMessage(), 'code' => 400)));
 		}
     	       
-	}
-	else
-	{
-		header("HTTP/1.1 500 Internal Server Error");
-		header('Content-Type: application/json; charset=UTF-8');
-		die(json_encode(array('message' => 'query errata', 'code' => 400)));
-	}
+	}else{
+    			header("HTTP/1.1 500 Internal Server Error");
+                header('Content-Type: application/json; charset=UTF-8');
+                die(json_encode(array('message' => 'Query errata!', 'code' => 400)));
+    }
+	
+	
 }
 ?>
