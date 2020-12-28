f5afa4d9443f80186f66cd8b5c481076f676fb17
MassimoEsperto <em.decarlo@gmail.com>
2020-12-28 16:50:26 +0100

    Update login.php

--- src/web-services/login.php
+++ src/web-services/login.php
@@ -16,28 +16,10 @@ $sql = "select id_utente,username,email,ruolo,squadra from utenti where password
 
 $result = mysqli_query( $con , $sql );
 
-if(! $result ) 
-{
-   header("HTTP/1.1 500 Internal Server Error");
-   header('Content-Type: application/json; charset=UTF-8');
-   die(json_encode(array('message' => 'query fallita', 'code' => 400)));
-}
 
-if ($result->num_rows > 0) 
-{
-	$rows = array();
-    while($r = mysqli_fetch_assoc($result)) {
-        $rows[] = $r;
-	}
-
- 	echo json_encode(['data'=>$rows[0]]);
-
-} 
-else 
-{
 	 header("HTTP/1.1 500 Internal Server Error");
      header('Content-Type: application/json; charset=UTF-8');
-     die(json_encode(array('message' => 'Dati errati!', 'code' => 400)));
-}
+     die(json_encode(array('message' => 'Versione Locale Obsoleta', 'code' => 400)));
+
 
 ?>
